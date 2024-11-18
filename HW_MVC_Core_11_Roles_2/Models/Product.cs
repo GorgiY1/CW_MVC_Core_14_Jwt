c3e2e3dd819f3ff4ce305099f1d2642cc5261c64
johnny <orgeorgij@gmail.com>
2024-11-18 14:56:11 +0200

    Add project files.

---
+++ HW_MVC_Core_11_Roles_2/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop_app.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "Id is required")]
+        public Guid Id { get; set; } = Guid.NewGuid(); // Задаем `Guid` для `Id`
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, MinimumLength = 2 , ErrorMessage = "min: 2 max: 100")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Price is required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.01,1000000.00, ErrorMessage = "min: 0.01 max: 1000000.00")]
+        public decimal Price { get; set; } = decimal.Zero;
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(1024, MinimumLength = 2, ErrorMessage = "min: 2 max: 1024")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
